1cf44b5a6212904d4728a6d7890fb9b88825eb2f
疯狂的狮子li <15040126243@163.com>
2019-12-14 14:57:55 +0800

    增加 springcloud-alibaba-dubbo 相关项目

--- alibaba-dubbo-server/src/main/java/com/alibaba/dubbo/server/service/TestServiceImpl.java
+++ alibaba-dubbo-server/src/main/java/com/alibaba/dubbo/server/service/TestServiceImpl.java
@@ -3,14 +3,16 @@ package com.alibaba.dubbo.server.service;
 import com.alibaba.dubbo.api.TestService;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * 测试接口实现
  */
 @Service // 这里使用的是dubbo的 @Service 注解 将注册到注册中心
+@RefreshScope // nacos 配置自动刷新
 public class TestServiceImpl implements TestService {
 
-    @Value("server.port:")
+    @Value("${server.port:}")
     private String port;
 
     @Override
