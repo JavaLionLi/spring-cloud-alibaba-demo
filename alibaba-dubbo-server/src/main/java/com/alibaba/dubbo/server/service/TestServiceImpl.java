45c9259b21e0830b378fed247612ee408ed0ff9f
疯狂的狮子li <15040126243@163.com>
2019-12-14 14:32:59 +0800

    增加 springcloud-alibaba-dubbo 相关项目

--- alibaba-dubbo-server/src/main/java/com/alibaba/dubbo/server/service/TestServiceImpl.java
+++ alibaba-dubbo-server/src/main/java/com/alibaba/dubbo/server/service/TestServiceImpl.java
@@ -2,6 +2,7 @@ package com.alibaba.dubbo.server.service;
 
 import com.alibaba.dubbo.api.TestService;
 import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * 测试接口实现
@@ -9,9 +10,12 @@ import org.apache.dubbo.config.annotation.Service;
 @Service // 这里使用的是dubbo的 @Service 注解 将注册到注册中心
 public class TestServiceImpl implements TestService {
 
+    @Value("server.port:")
+    private String port;
+
     @Override
     public String test(String name) {
-        return "服务器2::返回值 => " + name;
+        return "服务器::端口:"+port+"::返回值 => " + name;
     }
 
 }
